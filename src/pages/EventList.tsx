15ccb94241f81d86d8a01b3a76797c57b441ac37
Argen <sairbekow@gmail.com>
2023-06-17 20:02:11 +0600

    add: home page

---
+++ src/pages/EventList.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import Card from '../components/Card'
+
+const Wrapper = styled.div`
+  padding: 30px;
+`
+
+const Title = styled.h2`
+  font-size: 20px;
+`
+
+const Ul = styled.div`
+  margin-top: 30px;
+  display: grid;
+  grid-template-columns: 30fr 30fr 30fr;
+  grid-gap: 25px;
+`
+
+const EventList = () => {
+  const cards = [1, 2, 3, 4, 5, 6]
+  return (
+    <Wrapper>
+      <Title>Негизги иш-чаралар</Title>
+      <Ul>
+        {cards.map((item) => (
+          <li key={item}>
+            <Card />
+          </li>
+        ))}
+      </Ul>
+    </Wrapper>
+  )
+}
+
+export default EventList
